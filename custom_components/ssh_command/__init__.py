07c2a4140335e94db707ec1de2f0a2ec32c38440
Alex X <alexey.khit@gmail.com>
2024-12-07 10:04:30 +0300

    Rewrite service call function

--- custom_components/ssh_command/__init__.py
+++ custom_components/ssh_command/__init__.py
@@ -13,9 +13,11 @@ DEFAULT_SCHEMA = vol.Schema(
     {
         vol.Optional("host", default="172.17.0.1"): cv.string,
         vol.Optional("port", default=22): cv.port,
-        vol.Optional("user", default="pi"): cv.string,
-        vol.Optional("pass", default="raspberry"): cv.string,
-        vol.Optional("timeout", default=5): cv.positive_int,
+        vol.Optional("user", default="root"): cv.string,
+        vol.Optional("pass"): cv.string,
+        vol.Optional("private_key"): vol.Any(cv.string, cv.ensure_list),
+        vol.Optional("passphrase"): cv.string,
+        vol.Optional("timeout"): cv.positive_int,
     },
     extra=vol.PREVENT_EXTRA,
 )
@@ -24,51 +26,42 @@ CONFIG_SCHEMA = vol.Schema({DOMAIN: DEFAULT_SCHEMA}, extra=vol.ALLOW_EXTRA)
 
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
-    default = config[DOMAIN] if DOMAIN in config else DEFAULT_SCHEMA({})
+    default = config.get(DOMAIN) or DEFAULT_SCHEMA({})
 
     def exec_command(call: ServiceCall) -> dict:
-        host = call.data.get("host", default["host"])
-        port = call.data.get("port", default["port"])
-        username = call.data.get("user", default["user"])
-        password = call.data.get("pass", default["pass"])
-        timeout = call.data.get("timeout", default["timeout"])
-        command = call.data["command"]
+        kwargs = default | call.data
+        kwargs["hostname"] = kwargs.pop("host")
+        kwargs["username"] = kwargs.pop("user")
+        kwargs["password"] = kwargs.pop("pass", None)
+        kwargs["key_filename"] = kwargs.pop("private_key", None)
+
+        command = kwargs.pop("command")
 
         client = SSHClient()
         client.set_missing_host_key_policy(AutoAddPolicy())
 
         try:
-            if private_key := call.data.get("private_key"):
-                client.connect(
-                    host,
-                    port,
-                    username,
-                    key_filename=private_key,
-                    passphrase=call.data.get("passphrase"),
-                )
-            else:
-                # Use password for authentication if SSH key is not provided
-                client.connect(host, port, username, password, timeout=timeout)
-        except Exception as e:
-            _LOGGER.error(f"Failed to connect: {repr(e)}")
-            return {"error": repr(e)}
+            client.connect(**kwargs)
 
-        try:
-            _, stdout, stderr = client.exec_command(command, timeout=timeout)
+            _, stdout, stderr = client.exec_command(
+                command, timeout=kwargs.get("timeout")
+            )
+
+            return {
+                "stdout": stdout.read().decode("utf-8"),
+                "stderr": stderr.read().decode("utf-8"),
+            }
+
         except TimeoutError as e:
             _LOGGER.error(f"Command execution timeout")
             return {"error": repr(e)}
 
-        response = {
-            "command": command,
-            "stdout": stdout.read().decode("utf-8"),
-            "stderr": stderr.read().decode("utf-8"),
-        }
-        client.close()
+        except Exception as e:
+            _LOGGER.error(f"Failed to connect: {repr(e)}")
+            return {"error": repr(e)}
 
-        _LOGGER.debug(response)
-
-        return response
+        finally:
+            client.close()
 
     try:
         # ServiceResponse from Hass 2023.7
